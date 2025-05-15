cf717c7109bed0757dcbecd5a80a9fcc329fa886
unknown <ray-vinod>
2025-05-15 14:40:14 +0545

    Import and Export working on logic

---
+++ Views/UserControls/ImportExport.xaml.cs
@@ -0,0 +1,21 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MovieLibrary.Views.UserControls;
+
+public partial class ImportExport : UserControl
+{
+
+    public ImportExport()
+    {
+        InitializeComponent();
+    }
+
+    public void ImportButton_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    public void ExportButton_Click(object sender, RoutedEventArgs e)
+    {
+    }
+}
